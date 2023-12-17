50dbdeb6582e39672f261a7494dedf289db26a10
Bishwajitkuat <bishwajitkuat@gmail.com>
2023-12-17 12:54:22 +0200

    Checking url, method and headers of request object

--- server_http/app.js
+++ server_http/app.js
@@ -1,7 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  // URL
+  console.log("URL");
+  console.log(req.url);
+  // METHOD
+  console.log("METHOD");
+  console.log(req.method);
+  // HEADERS
+  console.log("HEADERS");
+  console.log(req.headers);
+  res.end();
 });
 
 server.listen("3000", () => console.log("listening to port 3000"));
