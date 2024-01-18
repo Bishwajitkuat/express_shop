7851bf587eb3dc2ad36b5b6bf2c32a1550e16e98
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-01-18 15:51:55 +0200

    product data and docTitle is passed into pug file

--- express_shop/routes/shopRoute.js
+++ express_shop/routes/shopRoute.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const path = require("path");
 const rootDir = require("../lib/paths");
+const { products } = require("../routes/adminRoute");
 
 const shopRoute = express.Router();
 
 shopRoute.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  // for sending static html file
+  // res.sendFile(path.join(rootDir, "views", "shop.html"));
+  // for sending pug file response, we provied path from default path of pug templating engine
+  console.log(products);
+  res.render("./shop.pug", { data: products, docTitle: "Shop" });
 });
 
 module.exports = shopRoute;
