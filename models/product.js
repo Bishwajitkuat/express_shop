05abe3d6841339a18493c2ffcadd0dda4c6e4ba9
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-01-19 01:42:55 +0200

    Product class is created which will be used to create new product and wirte into json file and get all products from json file

--- models/product.js
+++ models/product.js
@@ -1,4 +1,7 @@
-const products = [{ title: "Book" }, { title: "Mobile" }];
+const fs = require("fs");
+const path = require("path");
+const productsFile = path.join(__dirname, "data", "products.json");
+// const products = [{ title: "Book" }, { title: "Mobile" }];
 
 class Product {
   constructor(title) {
@@ -6,11 +9,29 @@ class Product {
   }
 
   save() {
-    products.push(this);
+    fs.readFile(productsFile, (err, data) => {
+      let products = [];
+      // only read file if there is no error
+      if (!err) {
+        // reading data from file
+        products = JSON.parse(data);
+      }
+      // appending new object
+      products.push(this);
+      // // writing back to file
+      fs.writeFile(productsFile, JSON.stringify(products), (err) =>
+        console.log(err)
+      );
+    });
   }
 
-  static getAllProducts() {
-    return products;
+  static async getAllProducts() {
+    try {
+      const data = await fs.promises.readFile(productsFile, "utf-8");
+      return JSON.parse(data);
+    } catch (err) {
+      return [];
+    }
   }
 }
 
