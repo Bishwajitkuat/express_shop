45579e2b94cc91e0ca5705412aa259b9f75dd78b
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-01-19 21:14:54 +0200

    A POST route for /cart is added in shopRoute
    postCart controller is registered for this route

--- routes/shopRoute.js
+++ routes/shopRoute.js
@@ -3,6 +3,7 @@ const {
   getProducts,
   getHomePage,
   getCart,
+  postCart,
   getCheckout,
   getOrders,
   getProductsById,
@@ -13,6 +14,7 @@ const shopRoute = express.Router();
 shopRoute.get("/products", getProducts);
 shopRoute.get("/products/:productId", getProductsById);
 shopRoute.get("/cart", getCart);
+shopRoute.post("/cart", postCart);
 shopRoute.get("/checkout", getCheckout);
 shopRoute.get("/orders", getOrders);
 shopRoute.get("/", getHomePage);
