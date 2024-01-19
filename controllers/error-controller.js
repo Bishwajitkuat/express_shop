2100e4a72217732729989b9c2f929a3b328a0190
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-01-19 15:19:39 +0200

    bug fix: get404 controller was not passing path value to view but view was needed it

--- controllers/error-controller.js
+++ controllers/error-controller.js
@@ -1,4 +1,6 @@
 exports.get404 = (req, res, next) => {
   // response in ejs templeting engine
-  res.status(404).render("./404.ejs", { docTitle: "404 | page not found" });
+  res
+    .status(404)
+    .render("./404.ejs", { docTitle: "404 | page not found", path: false });
 };
