522ade6755ce78fcb35ef877f8e71fd0f6c0f688
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-01-19 21:32:20 +0200

    postCart controller is written to handel POST reques in /cart route
    productId is extracted from request body and redirected to /cart route

--- controllers/shop-controller.js
+++ controllers/shop-controller.js
@@ -32,6 +32,12 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.postCart = (req, res, next) => {
+  const productId = req.body.productId;
+  console.log(productId);
+  res.redirect("/cart");
+};
+
 exports.getCheckout = (req, res, next) => {
   res.render("./shop/checkout.ejs", {
     docTitle: "Checkout",
