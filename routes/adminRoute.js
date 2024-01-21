c4aa452d0a25309ddbaec1b3ddad9ef0a2ceca66
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-01-21 13:33:00 +0200

    /edit-product/:productId route is added to adminRoute.js
     We can pass productId as params in this get rout. and getEditProduct contoller is registered for this route

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -4,15 +4,18 @@ const {
   getAddProduct,
   postAddProduct,
   getProducts,
+  getEditProduct,
 } = require("../controllers/admin-controller");
 
 const adminRoute = express.Router();
-
+// receiving GET request
 adminRoute.get("/add-product", getAddProduct);
+// receiving POST request
+adminRoute.post("/add-product", postAddProduct);
+
+// GET request for editing the product
+adminRoute.get("/edit-product/:productId", getEditProduct);
 
 adminRoute.get("/products", getProducts);
-
-// receiving post request
-adminRoute.post("/add-product", postAddProduct);
 
 module.exports = adminRoute;
