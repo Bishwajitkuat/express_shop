b4a8058500f070992647c3dbacd5fde76ed4026f
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-01-23 00:24:53 +0200

    Product schema is written with define() method from sequelize and removed Product class

--- models/product.js
+++ models/product.js
@@ -1,52 +1,33 @@
-const db = require("../lib/database");
+"use strict";
+const sqzType = require("sequelize");
+const sequelize = require("../lib/database");
 
-class Product {
-  constructor(title, imgUrl, description, price) {
-    this.title = title;
-    this.imgUrl = imgUrl;
-    this.description = description;
-    this.price = Number(price);
-  }
-
-  save() {
-    return db.execute(
-      `INSERT INTO products (title, price, imgUrl, description) VALUES (?, ?, ?, ?);`,
-      [this.title, this.price, this.imgUrl, this.description]
-    );
-  }
-
-  static updateProduct(updatedProduct) {
-    return db.execute(
-      "UPDATE products SET title=?, price=?, imgUrl=?, description=? WHERE id=?;",
-      [
-        updatedProduct.title,
-        Number(updatedProduct.price),
-        updatedProduct.imgUrl,
-        updatedProduct.description,
-        updatedProduct.id,
-      ]
-    );
-  }
-
-  static deleteProduct(id) {
-    return db.execute("DELETE FROM products WHERE id=?;", [id]);
-  }
-
-  static getAllProducts() {
-    return new Promise((resolve, reject) => {
-      db.execute("SELECT * FROM products;")
-        .then((res) => resolve(res[0]))
-        .catch((err) => resolve(false));
-    });
-  }
-
-  static getProductById(id) {
-    return new Promise((resolve, reject) => {
-      db.execute(`SELECT * FROM products WHERE id=${id};`)
-        .then((res) => resolve(res[0][0]))
-        .catch((err) => resolve(false));
-    });
-  }
-}
+// creating schema for Product, define methods takes first argument as table name and object as second argument
+// the object's property name is the field name of the table
+// each property holds an object which contains various charectaristics of the fileds
+const Product = sequelize.define("product", {
+  id: {
+    type: sqzType.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  title: {
+    type: sqzType.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: sqzType.DOUBLE,
+    allowNull: false,
+  },
+  imgUrl: {
+    type: sqzType.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: sqzType.STRING,
+    allowNull: false,
+  },
+});
 
 module.exports = Product;
