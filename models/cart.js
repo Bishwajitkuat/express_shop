8ed3ec8debf0acda58fb4a92c64e67489c5caa69
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-01-22 17:00:06 +0200

    getCart and removeProduct static methods are added to Cart class
    getCart methods return a promise, if operation is a success, it resolve into cart object. If it failes, it will resolve into a cart object too but products array of cart will be empty
    
    removeProduct method receives product id and quantity as parameter. It will substract quantity from product.qty, if the resulted quantity is grater than 0, it will substract 1 from product.qty, otherwise it will remove the product from cart

--- models/cart.js
+++ models/cart.js
@@ -3,6 +3,19 @@ const path = require("path");
 const filePath = path.join(__dirname, "data", "cart.json");
 
 class Cart {
+  static getCart = () => {
+    return new Promise((resolve, reject) => {
+      let cart = { products: [], totalPrice: 0, totalQty: 0 };
+      try {
+        fs.readFile(filePath, (err, data) => {
+          cart = JSON.parse(data);
+          resolve(cart);
+        });
+      } catch (err) {
+        resolve(cart);
+      }
+    });
+  };
   static addProduct = (id, productPrice) => {
     return new Promise((resolve, reject) => {
       const price = Number(productPrice);
@@ -59,6 +72,60 @@ class Cart {
       });
     });
   };
+  // it can be remove qty one by one or all togather
+  static removeProduct(id, quantity) {
+    const qty = Number(quantity);
+    return new Promise((resolve, reject) => {
+      try {
+        fs.readFile(filePath, (err, data) => {
+          let cart = JSON.parse(data);
+          let updatedProducts = [...cart.products];
+
+          const productToRemove = cart.products.find((item) => item.id === id);
+          // removing the product from cart if the qty goes smaller than 1
+          if (productToRemove.qty - qty < 1) {
+            updatedProducts = cart.products.filter((item) => item.id !== id);
+          }
+          // if the qty after substraction remain grater than 0, qty will reduce by 1
+          else if (qty === 1 && productToRemove.qty - qty > 0) {
+            updatedProducts = updatedProducts.reduce((acc, item) => {
+              if (item.id === id) {
+                const newQty = item.qty - 1;
+                item.qty = newQty;
+                // acc.push(item);
+                return [...acc, item];
+              }
+              return [...acc, item];
+            }, []);
+          }
+          // updating cart info after change
+          // updating totalPrice
+          let updatedTotalPrice = 0;
+          // updated total quantity
+          let updatedTotalQty = 0;
+
+          for (let item of updatedProducts) {
+            updatedTotalPrice =
+              updatedTotalPrice + Number(item.price) * Number(item.qty);
+            updatedTotalQty = updatedTotalQty + Number(item.qty);
+          }
+          const updatedCart = {
+            products: updatedProducts,
+            totalPrice: updatedTotalPrice,
+            totalQty: updatedTotalQty,
+          };
+          // writing back
+          fs.writeFile(filePath, JSON.stringify(updatedCart), (err) => {
+            if (err) resolve("failed");
+            else resolve("success");
+          });
+        });
+      } catch (err) {
+        console.log(err);
+        resolve("failed");
+      }
+    });
+  }
 }
 
 module.exports = Cart;
