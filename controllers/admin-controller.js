825d2137e4b6b809847a9d5b6457e1800b02ec01
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-01-23 01:53:12 +0200

    save() method of entry of products table is used to save the updated product in postEditProduct controllers

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -39,20 +39,25 @@ exports.getEditProduct = (req, res, next) => {
   }
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const id = req.body.id;
-  Product.deleteProduct(id)
+exports.postEditProduct = (req, res, next) => {
+  const updatedProduct = req.body;
+  // save() method of entry will save the updated entry
+  Product.findByPk(updatedProduct.id)
+    .then((product) => {
+      product.title = updatedProduct.title;
+      product.price = Number(updatedProduct.price);
+      product.imgUrl = updatedProduct.imgUrl;
+      product.description = updatedProduct.description;
+      return product.save();
+    })
     .then((response) => res.redirect("/admin/products"))
     .catch((err) => console.log(err));
 };
 
-exports.postEditProduct = (req, res, next) => {
-  const updatedProduct = req.body;
-  // updateProduct method of Product classe will replace the old product with the updatedProduct
-  Product.updateProduct(updatedProduct)
-    .then((response) => {
-      res.redirect("/admin/products");
-    })
+exports.postDeleteProduct = (req, res, next) => {
+  const id = req.body.id;
+  Product.deleteProduct(id)
+    .then((response) => res.redirect("/admin/products"))
     .catch((err) => console.log(err));
 };
 
