19f3c4e177e3f5de1ffb708b0ed9225bfc3abd37
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-01-23 16:34:35 +0200

    Model for User is created with sequelize

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const sqzType = require("sequelize");
+const sequelize = require("../lib/database");
+
+const User = sequelize.define("users", {
+  id: {
+    type: sqzType.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: sqzType.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: sqzType.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
