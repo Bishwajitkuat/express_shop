4f3ff5ce938082e6d8854d61fd8379881a5d23fc
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-02-09 21:53:38 +0200

    Removed code for sequelize Created Product class Added save method to class it will insert entry to products collection.

--- models/product.js
+++ models/product.js
@@ -1,33 +1,17 @@
 "use strict";
-const sqzType = require("sequelize");
-const db = require("../lib/database");
+const db = require("../lib/database").getDB;
 
-// creating schema for Product, define methods takes first argument as table name and object as second argument
-// the object's property name is the field name of the table
-// each property holds an object which contains various charectaristics of the fileds
-const Product = db.define("product", {
-  id: {
-    type: sqzType.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-    allowNull: false,
-  },
-  title: {
-    type: sqzType.STRING,
-    allowNull: false,
-  },
-  price: {
-    type: sqzType.DOUBLE,
-    allowNull: false,
-  },
-  imgUrl: {
-    type: sqzType.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: sqzType.STRING,
-    allowNull: false,
-  },
-});
+class Product {
+  constructor(title, price, imgUrl, description) {
+    this.title = title;
+    this.price = price;
+    this.imgUrl = imgUrl;
+    this.description = description;
+  }
+  async save() {
+    const response = await db().collection("products").insertOne(this);
+    return response;
+  }
+}
 
 module.exports = Product;
