d190f93e26f95915bd033495ae06a1bd75044931
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-02-11 15:52:06 +0200

    cart property, and getCart and updateCart static methods are added to User class. cart object will be created when an instance of User class is created. getCart method will fetch the cart object associated to the userId. updateCart method will update the cart object associated to the userId with updated cart object.

--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@ class User {
   constructor(name, email) {
     this.name = name;
     this.email = email;
+    this.cart = { items: [], totalQuantity: 0, totalPrice: 0 };
   }
   async save() {
     return await db().collection("users").insertOne(this);
@@ -14,6 +15,17 @@ class User {
     return await db()
       .collection("users")
       .findOne({ _id: new ObjectId(id) });
+  }
+  static async getCart(userId) {
+    // fetch cart object for the userId passed as parameter
+    const user = await db().collection("users").findOne({ _id: userId });
+    return user.cart;
+  }
+  static async updateCart(userId, updatedCart) {
+    // update new cart into db.
+    await db()
+      .collection("users")
+      .updateOne({ _id: userId }, { $set: { cart: updatedCart } });
   }
 }
 
