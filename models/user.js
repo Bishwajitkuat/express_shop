826c76c80190e938569e9807d4bcd2ee8a1282e9
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-02-13 22:55:21 +0200

    addToCart utility method is added to User Modle. It only be accessible from any instance of User model. It takes product id as string as argument. If the product does not exist into items array, It creates an object with productId and quantity properties and push it to items array. If the product already exists, it increases the quantity value by 1. Finally it save back the updated info into db.

--- models/user.js
+++ models/user.js
@@ -26,4 +26,27 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
+// adding utility method to instances created from User model, arrow function does not work here
+UserSchema.methods.addToCart = function (productId) {
+  // whether the product already existed in the items array, if does not exist productIndex value will be -1
+  const productIndex = this.cart.items.findIndex(
+    (item) => item.productId.toString() === productId.toString()
+  );
+  let updatedItem = {};
+  if (productIndex === -1) {
+    // this product does not exist in the items array, so adding productId and quantity values
+    updatedItem.productId = productId;
+    updatedItem.quantity = 1;
+    this.cart.items.push(updatedItem);
+  } else {
+    // the product already existed in the items array so only updating the quantity
+    updatedItem = this.cart.items[productIndex];
+    updatedItem.quantity += 1;
+    // replacing the old itme with updated item
+    this.cart.items.splice(productIndex, updatedItem);
+  }
+  // saving back to the db.
+  return this.save();
+};
+
 module.exports = mongoose.model("User", UserSchema);
