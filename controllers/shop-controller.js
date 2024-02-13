73180e78b8a09b65a91d73311f0b0a19558125af
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-02-13 22:57:27 +0200

    postAddToCart controller is modified to use addToCart utility method of User model. addToCart method takes productId as argument and updates the cart object accordingly.

--- controllers/shop-controller.js
+++ controllers/shop-controller.js
@@ -60,52 +60,10 @@ exports.getCart = async (req, res, next) => {
 
 exports.postAddToCart = async (req, res, next) => {
   try {
-    const { productId, fromCart } = req.body;
-    const userId = req.user._id;
-    const cart = await User.getCart(userId);
-    let items = cart.items;
-
-    // checking whether the product already in items array in cart
-    // tow object never be same, so i am converting mongo db id object to string to compare.
-    const indexInItemsArryOfCart = items.findIndex(
-      (item) => item._id.toString() == productId
-    );
-
-    if (indexInItemsArryOfCart === -1) {
-      // this product does not exists in item array of cart
-      // we need to fetch the product, add quantity to the product and push into items array
-      const product = await Product.getProductById(productId);
-      product.quantity = 1;
-      items.push(product);
-    } else {
-      // product is already exists we need to adjust the quantity of the product
-      const updatedProduct = items[indexInItemsArryOfCart];
-      updatedProduct.quantity += 1;
-      items[indexInItemsArryOfCart] = updatedProduct;
-    }
-    // calculating updated totalQuantity and totalPrice
-    let totalQuantity = 0;
-    let totalPrice = 0;
-    for (let item of items) {
-      totalQuantity += item.quantity;
-      totalPrice += item.quantity * item.price;
-    }
-    // updating cart with updated totalQuantity and totalPrice
-    const updatedCart = {
-      items,
-      totalQuantity,
-      totalPrice,
-    };
-    // storing updated cart into db
-    User.updateCart(userId, updatedCart).then((response) => {
-      // if user add items from cart view it will riderect to cart view
-      if (fromCart === "yes") {
-        res.redirect("/cart");
-      } else {
-        // if user add items from other pages it will be redirected to /products route
-        res.redirect("/products");
-      }
-    });
+    const { productId } = req.body;
+    const user = req.user;
+    // utility method of User model is used to add or increase the quanity of a product
+    user.addToCart(productId).then((response) => res.redirect("/products"));
   } catch (err) {
     console.log(err);
     res.redirect("/products");
