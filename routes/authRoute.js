6295f64582bee6d8a4a3501b387d44af4ea1c401
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-02-15 22:35:08 +0200

    postLogOut is registered to handle POST request to /logout route.

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { getLogin, postLogin } = require("../controllers/auth-controller");
+const {
+  getLogin,
+  postLogin,
+  postLogOut,
+} = require("../controllers/auth-controller");
 
 const router = express.Router();
 // get login page
 router.get("/login", getLogin);
 // post /login route
 router.post("/login", postLogin);
+// post /logout
+router.post("/logout", postLogOut);
 module.exports = router;
