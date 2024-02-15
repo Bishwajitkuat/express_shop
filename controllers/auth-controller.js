2d5c2dcb2c085c52a29f888ee757c9606a7fcfbf
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-02-15 13:58:18 +0200

    postLogin controller funtion is declered. It will handle POST request to /login route. It sets cooke isLoggedIn=ture. and redirect user to home page.

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,4 +1,10 @@
-// controllers for handling get request to login page
+// controller for handling GET request to /login route
 exports.getLogin = (req, res, next) => {
   res.render("./auth/login.ejs", { docTitle: "Login", path: "/login" });
+};
+// controller for handling POST request to /login route
+exports.postLogin = (req, res, next) => {
+  // setting cookie with setHeader
+  res.setHeader("Set-Cookie", "isLoggedIn=true");
+  res.redirect("/");
 };
