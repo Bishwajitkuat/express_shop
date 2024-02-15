0597c5d61ceb6089be9f455b920e9f83c1302527
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-02-15 15:06:29 +0200

    isLoggedIn cookie value is extracted and passed into view for conditional rendering.

--- controllers/error-controller.js
+++ controllers/error-controller.js
@@ -1,6 +1,14 @@
+const { getIsLoggedInFromCooke } = require("../lib/cookie-extractor");
+
 exports.get404 = (req, res, next) => {
+  // using helper function to extract isLoggedIn cookie value
+  const isLoggedIn = getIsLoggedInFromCooke(req.get("Cookie"));
   // response in ejs templeting engine
   res
     .status(404)
-    .render("./404.ejs", { docTitle: "404 | page not found", path: false });
+    .render("./404.ejs", {
+      docTitle: "404 | page not found",
+      path: false,
+      isLoggedIn,
+    });
 };
