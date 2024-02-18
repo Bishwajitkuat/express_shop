3865d72f838cd1e21e714bdf76698df76939ad2a
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-02-18 20:24:19 +0200

    passwordResetToken and passwordResetToken properties are added to UserSchema.

--- models/user.js
+++ models/user.js
@@ -28,6 +28,9 @@ const UserSchema = new mongoose.Schema({
       },
     ],
   },
+  passwordResetToken: String,
+
+  resetTokenExpiration: Date,
 });
 
 // adding utility method to instances created from User model, arrow function does not work here
